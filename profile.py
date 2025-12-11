eb4cf6939d49125b3e9794f13dc4a7cfae857843
Carlos Ruben Dell'Aquila <carlos.dellaquila@cern.ch>
2025-12-11 13:11:13 +0100

    changes revert to previous commit

--- profile.py
+++ profile.py
@@ -138,11 +138,7 @@ for nodeName in nodeList:
 
     link_vlan_1.addInterface(fpga_iface1)
     link_vlan_2.addInterface(fpga_iface2)
-
-    if not(i%2):
-        link_vlan_1.addInterface(host_iface1)
-    else:
-        link_vlan_2.addInterface(host_iface1)
+    lan.addInterface(host_iface1)
   
     i+=1
 
