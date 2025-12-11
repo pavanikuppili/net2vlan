bb0be221f1a4a4174fc42869ab098e08ae11d1e7
Carlos Ruben Dell'Aquila <carlos.dellaquila@cern.ch>
2025-12-11 13:02:42 +0100

    Changes the rule to set a vlan to the host pc interface

--- profile.py
+++ profile.py
@@ -138,7 +138,11 @@ for nodeName in nodeList:
 
     link_vlan_1.addInterface(fpga_iface1)
     link_vlan_2.addInterface(fpga_iface2)
-    lan.addInterface(host_iface1)
+
+    if not(i%2):
+        link_vlan_1.addInterface(host_iface1)
+    else:
+        link_vlan_2.addInterface(host_iface1)
   
     i+=1
 
