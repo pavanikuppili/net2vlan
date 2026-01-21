d677ffcbb5d15bcc3759f56d5bcbbe9eab3eb2ff
Carlos Ruben Dell'Aquila <carlos.dellaquila@cern.ch>
2026-01-21 14:38:53 +0100

    change eth name

--- profile.py
+++ profile.py
@@ -117,8 +117,8 @@ for nodeName in nodeList:
 
     if i == 0:
         host_iface1 = host.addInterface()
-        host_iface1.component_id = "eth1"
-        host_iface1.addAddress(pg.IPv4Address("192.168.40." + str(i+20), "255.255.255.0"))
+        host_iface1.component_id = "eth3"
+        host_iface1.addAddress(pg.IPv4Address("192.168.40." + str(i+25), "255.255.255.0"))
 
 
     if i == 0:
